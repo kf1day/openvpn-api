726353310ca6dc781e47fe5fb1774ffca6ba3673
Andrew Day <launchmyrocketsaway@gmail.com>
2021-05-24 02:06:17 +0300

    move config files to conf directory

--- init.sh
+++ init.sh
@@ -4,5 +4,5 @@ DIR="`dirname $0`"
 
 test -d "${DIR}/cert" || mkdir "${DIR}/cert"
 test -d "${DIR}/crldb" || ( mkdir "${DIR}/crldb"; : > "${DIR}/crldb/index.txt"; echo "unique_subject = yes" > "${DIR}/crldb/index.txt.attr"; echo "00" > "${DIR}/crldb/number" )
-test -f "${DIR}/vars.conf" || cp "${DIR}/vars.conf.sample" "${DIR}/vars.conf"
-test -f "${DIR}/export.conf" || cp "${DIR}/export.conf.sample" "${DIR}/export.conf"
+test -f "${DIR}/conf/vars.conf" || cp "${DIR}/conf/vars.conf.sample" "${DIR}/conf/vars.conf"
+test -f "${DIR}/conf/export.conf" || cp "${DIR}/conf/export.conf.sample" "${DIR}/conf/export.conf"
--- webroot/test.cgi
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo 'Content-Type: text/plain; charset=utf-8'
-echo
-
-RUN="`dirname ${SCRIPT_FILENAME}`"
-DIR="`dirname ${RUN}`"
-set
